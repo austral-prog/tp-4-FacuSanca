29fbba8511d66a272bf9792ae2d97357a2ece1db
FacuSanca <fsanca@mail.austral.edu.ar>
2025-04-16 16:45:12 -0300

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,8 @@
 def leap_year():
-    print("TO DO")
+    a = int(input("Ingrese un año: "))
+    b = a % 4 
+    c = a % 400 or a % 4
+    if b == 0 and c == 0: 
+        print(f"El año {a} es bisiesto")
+    else: 
+        print(f"El año {a} no es bisiesto")
